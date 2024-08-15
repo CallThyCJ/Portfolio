33c8c893dd1070eae88faf8db4451c50d3fb6468
CallThyCJ <135438020+CallThyCJ@users.noreply.github.com>
2024-08-15 11:41:59 +0100

    Type Writer Effect All Pages
    Added the type writter effect to all pages. Changed the JS to appy the effect based on what page the HTML is in.

--- JS/textScroll.js
+++ JS/textScroll.js
@@ -1,40 +1,65 @@
 const textTarget = document.querySelector(".jsText");
 const titles = ["Web Developer", "Software Developer", "Code Enjoyer"];
 let sleepTime = 100;
+let currentWord;
 let currentTitleIndex = 0;
+const currentPage = window.location.pathname;
 
 // If JS is working then get rid of HTML title
 document.querySelector(".htmlText").style.display = "none";
 
-// typewritter effect for titles
+// Sleep Delay
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// Typewriter effect function
+const typeWriterEffect = async (text) => {
+    for (let i = 0; i < text.length; i++) {
+        textTarget.innerText = text.substring(0, i + 1);
+        await sleep(sleepTime);
+    }
+};
+
+// Backspace effect function
+const backspaceEffect = async (text) => {
+    for (let i = text.length; i > 0; i--) {
+        textTarget.innerText = text.substring(0, i - 1);
+        await sleep(sleepTime);
+    }
+};
+
+// Index Page Loop
 const writeLoop = async () => {
     while (true) {
         let currentWord = titles[currentTitleIndex];
 
-        for (let i = 0; i < currentWord.length; i++) {
-            textTarget.innerText = currentWord.substring(0, i + 1);
-            await sleep(sleepTime);
-        }
-        
+        await typeWriterEffect(currentWord);
         await sleep(sleepTime * 10);
-
-        for (let i = currentWord.length; i > 0; i--) {
-            textTarget.innerText = currentWord.substring(0, i - 1);
-            await sleep(sleepTime);
-        }
-
+        await backspaceEffect(currentWord);
         await sleep(sleepTime * 5);
 
-        if (currentTitleIndex === titles.length - 1) {
-            currentTitleIndex = 0;
-        } else {
-            currentTitleIndex++;
-        }
+        currentTitleIndex = (currentTitleIndex + 1) % titles.length;
     }
 };
 
-writeLoop();
+// Start the effect based on the page
+if (currentPage.includes("index.html")) {
+    writeLoop();
+    } else if (currentPage.includes("about-me.html")) {
+        const runAboutMeEffect = async () => {
+            await typeWriterEffect("About Me");
+        };
+    runAboutMeEffect();
+    } else if (currentPage.includes("code-examples.html")) {
+        const runCodeEGEffect = async () => {
+            await typeWriterEffect("Code Examples");
+        };
+    runCodeEGEffect();
+    } else if (currentPage.includes("scs.html")) {
+        const runScsEffect = async () => {
+            await typeWriterEffect("SCS Scheme");
+        };
+    runScsEffect();
+
+}
