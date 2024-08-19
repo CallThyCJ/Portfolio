c826df89e2e18d3dfdbafbb4abd8ef3d262ff7b2
CallThyCJ <135438020+CallThyCJ@users.noreply.github.com>
2024-08-19 15:29:17 +0100

    Small Transitions fix

--- JS/transition.js
+++ JS/transition.js
@@ -11,12 +11,13 @@ for (let i = 0; i < anchors.length; i++) {
 
     anchor.addEventListener("click", e => {
         e.preventDefault();
-        let target = e.target.href;
+        let target = e.currentTarget.href;
 
         transitionEl.classList.add("isActive");
 
         setTimeout(() => {
             window.location.href = target;
+            console.log(target);
         }, 500);
     });
 }
