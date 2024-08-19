beb6e7ad613d88394ced9d126a78472f8706d3a1
CallThyCJ <135438020+CallThyCJ@users.noreply.github.com>
2024-08-19 11:48:31 +0100

    TRANSITION EFFECTS
    added transitions with to my pages with JS and css

---
+++ JS/transition.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    const transitionEl = document.querySelector(".transition")
+    const anchors = document.querySelectorAll("a");
+
+    setTimeout(() => {
+        transitionEl.classList.remove("isActive");
+    }, 500);
+}
+
+for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+
+    anchor.addEventListener("click", e => {
+        e.preventDefault();
+        let target = e.target.href;
+
+        transitionEl.classList.add("isActive");
+
+        setTimeout(() => {
+            window.location.href = target;
+        }, 500);
+    });
+}
