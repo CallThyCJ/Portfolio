cf063b5d84df1e95559294c611b5211c3f0ce0e4
CallThyCJ <135438020+CallThyCJ@users.noreply.github.com>
2024-08-20 10:16:47 +0100

    Transition Funtionality Change
    Incorporated the capturing of the hash in the URL in order to help to detect when the user clicks an A link that leads to the same. Then if they do, instead of using the transition, the page will smooth scroll there. Then the transition is used if a new page is actually loaded

--- JS/transition.js
+++ JS/transition.js
@@ -1,24 +1,43 @@
+const transitionEl = document.querySelector(".transition");
+
 window.onload = () => {
-    const transitionEl = document.querySelector(".transition")
     const anchors = document.querySelectorAll("a");
+    console.log("page loaded");
 
     setTimeout(() => {
         transitionEl.classList.remove("isActive");
+        console.log("active removed");
     }, 500);
 
-for (let i = 0; i < anchors.length; i++) {
-    const anchor = anchors[i];
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
 
-    anchor.addEventListener("click", e => {
-        e.preventDefault();
-        let target = e.currentTarget.href;
+        anchor.addEventListener("click", e => {
+            const currentUrl = window.location.href.split('#')[0];
+            const targetUrl = anchor.href.split('#')[0];
+            const hash = anchor.hash;  // Get the hash part of the URL
 
-        transitionEl.classList.add("isActive");
+            if (currentUrl === targetUrl && hash) {
+                // Same page, different section
+                e.preventDefault();
 
-        setTimeout(() => {
-            window.location.href = target;
-            console.log(target);
-        }, 500);
-    });
-}
+                const targetElement = document.querySelector(hash);
+                if (targetElement) {
+                    // Smooth scroll to the section
+                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                }
+            } else {
+                // Different page, apply transition
+                e.preventDefault();
+                let target = e.currentTarget.href;
+
+                transitionEl.classList.add("isActive");
+
+                setTimeout(() => {
+                    window.location.assign(target);
+                    console.log(target);
+                }, 500);
+            }
+        });
+    }
 }
